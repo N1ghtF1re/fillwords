f655f26677bc84c785c2f0f0334a89fa1454d3fd
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2017-12-16 23:00:36 +0300

    opyat

--- units/Nikita.pas
+++ units/Nikita.pas
@@ -12,8 +12,8 @@ procedure PullCoordinates(const Fieldsize:integer; dictionary:TDictionary; var P
  exiter:=False;
   n:=fieldsize;
   repeat
-    ib:=4;//Random(n)+1;
-    jb:=4//Random(n)+1;
+    ib:=Random(n)+1;
+    jb:=Random(n)+1;
   until  (playingField[ib,jb])=' ';
   repeat
     direction:=random(1)+1;             //1-Right 2-Bottom 3-Left 4-up
