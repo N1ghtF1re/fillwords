f8c88400e70b1072bab0ee8e11bb19c25d0fe9b4
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2017-12-16 19:19:03 +0300

    New functional

--- fillwords.dpr
+++ fillwords.dpr
@@ -6,14 +6,17 @@ uses
   SysUtils,
   typesandconst in 'units\typesandconst.pas',
   Kiryl in 'units\Kiryl.pas',
+  Nikita in 'units\Nikita.pas',
   Sasha in 'units\Sasha.pas';
 
 var name:string;
     level:integer;
     Words:TDictionary;
+    PlayingField: TPlayingField;
 begin
   getConfig(name,level);
   Writeln('Welcome, ', name);
   fillArray(Words);
+  game(level,Words, PlayingField);
   Readln;
 end.
--- units/Sasha.pas
+++ units/Sasha.pas
@@ -9,7 +9,9 @@ var
    query:string;
    substr:string;
 const link = 'includes\config.txt';
+const letters = ['A'..'Z', 'a'..'z'];
 procedure getConfig(var name:string;var level:integer);
+procedure game(const lvl:integer; Words:TDictionary;var field: TPlayingField);
 
 implementation
 procedure getConfig(var name:string;var level:integer);
@@ -39,6 +41,40 @@ begin
   end;
 end;
 
+procedure game(const lvl:integer; Words:TDictionary;var field: TPlayingField);
+var fieldsize,cellsnum:integer;
+i,j:byte;
+var ib,jb,ie,je, badi, badj: integer;
+backup: TPlayingField;
+begin
+  fieldsize:= 5+lvl div 5;
+  writeln(fieldsize);
+  for i:=1 to fieldsize do
+  begin
+    for j:=1 to fieldsize do
+    begin
+      field[i,j]:=' ';
+    end;
+  end;
+  cellsnum:=fieldsize;
+
+  repeat
+    backup:=field;
+    PullCoordinates(FieldSize, Words,field, ib,jb,ie,je:Integer);
+    if ( not doItFilledOkay(field, fieldsize, badi, badj) ) then
+    begin
+      if (( field[i+1,j] = ' ') and ( field[i+2,j] <> ' ')) then
+        
+    end;
+
+  until(cellsnum <= 0);
+end;
+
 end.
+
+
+
+
+
 
 
