410206171c0b09d28c6bb097be6369b95c7f8a1d
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2017-12-16 17:47:32 +0300

    Edited main file

--- fillwords.dpr
+++ fillwords.dpr
@@ -5,11 +5,15 @@ program fillwords;
 uses
   SysUtils,
   typesandconst in 'units\typesandconst.pas',
+  Kiryl in 'units\Kiryl.pas',
   Sasha in 'units\Sasha.pas';
+
 var name:string;
     level:integer;
+    Words:TDictionary;
 begin
   getConfig(name,level);
   Writeln('Welcome, ', name);
+  fillArray(Words);
   Readln;
 end.
